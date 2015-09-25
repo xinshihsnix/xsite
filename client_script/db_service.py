945b7924860893740f9c543f98bf4fac88c3ec9f
xinshihsnix <xinshihsnix@sina.cn>
2015-09-25 17:57:17 +0800

    fix

--- client_script/db_service.py
+++ client_script/db_service.py
@@ -3,18 +3,19 @@ import MySQLdb
 
 
 class DBService(object):
-    def __init__(self, host, user, passwd, db, port=3306):
+    def __init__(self, host, user, passwd, db, port=3306, charset='utf8'):
         self.host = host
         self.user = user
         self.passwd = passwd
         self.db = db
         self.port = port
+        self.charset = charset
 
         self.conn = self.connect()
         self.cursor = self.conn.cursor()
 
     def connect(self):
-        conn=MySQLdb.connect(host=self.host, user=self.user, passwd=self.passwd, port=3306)
+        conn=MySQLdb.connect(host=self.host, user=self.user, passwd=self.passwd, port=3306, charset=self.charset)
         conn.select_db(self.db)
 
         return conn
@@ -25,3 +26,5 @@ class DBService(object):
     def close(self):
         self.cursor.close()
         self.conn.close()
+
+xsite_dbs = DBService('127.0.0.1', 'root', 'root', 'xsite')
--- client_script/xs.py
+++ client_script/xs.py
@@ -1,28 +1,58 @@
-# coding: utf-8
-"""
-参数说明:
-
-"""
+#!/usr/bin/python
+# -*- coding: utf8 -*-
 
 import sys
+from db_service import xsite_dbs
 
-COMPANY = 'keju'
 # print sys.argv
 
 argvs = [a.lower() for a in sys.argv]   # 忽略大小写
 
 option = argvs[1]
 
-title = argvs[2]
+title = argvs[2] if len(argvs) > 2 else ''
 
-content = argvs[3]
+content = argvs[3] if len(argvs) > 3 else ''
 
-if option == 'add':
+
+def handle_add():
     pass
 
-elif option == 's':
-    """ select """
+
+def handle_s():
     pass
 
-elif option == 'sa':
-    """ select all  """
+
+def handle_sa():
+    _sql = 'select * from reminder_reminder'
+    xsite_dbs.cursor.execute(_sql)
+    rows = xsite_dbs.cursor.fetchall()
+
+    for r in rows:
+        print r[2], ' ------ ',r[3]
+
+    xsite_dbs.close()
+
+options = [
+    {
+        'option': 'add',
+        'help_text': '',
+        'function': handle_add,
+    },
+    {
+        'option': 's',
+        'help_text': 'select',
+        'function': handle_s,
+    },
+    {
+        'option': 'sa',
+        'help_text': 'select all',
+        'function': handle_sa,
+    }
+]
+
+
+if __name__ == '__main__':
+    for op in options:
+        if option == op.get('option'):
+            op.get('function')()
