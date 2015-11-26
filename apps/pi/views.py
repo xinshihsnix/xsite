c95f5853d6023ed324d6e01db6db9f37dbabcfdd
xinshi <xinshihsnix@sina.cn>
2015-11-26 18:16:32 +0800

    eye improve

--- apps/pi/views.py
+++ apps/pi/views.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import json
+
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
@@ -10,5 +12,14 @@ from ..common.utils import time_now_str
 
 @xinshi_only()
 def read_eye(request):
-    image_data = open('/var/pi_media/{0}.jpg'.format(time_now_str()))
-    return HttpResponse(image_data,content_type="image/png")
+    type = request.GET.get('type')
+    try:
+        image_data = open('/var/pi_media/{0}.jpg'.format(time_now_str()))
+        # image_data.close()
+        if type == 'get_status':
+            return HttpResponse(json.dumps({'status': 'ready'}), content_type='application/json')
+        elif type == 'get_img':
+            return HttpResponse(image_data,content_type="image/png")
+    except Exception, e:
+        print 'read_eye:', e
+    return HttpResponse()
